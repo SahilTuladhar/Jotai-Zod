3353fab2bd13bbc6a171df035d33639b2b4c4c5a
SailTuladhar <sailratna11@gmail.com>
2025-04-07 15:19:27 +0545

    Form implemented using Zod

--- zod-app/src/Components/LoginInput.tsx
+++ zod-app/src/Components/LoginInput.tsx
@@ -1,27 +1,82 @@
 import styles from './LoginInput.module.css'
+import {useForm} from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod';
+import { resolve } from 'path'
+
+import {z} from 'zod'
+
+const FormSchema = z.object({
+    firstName : z.string().min(1 , 'first Name is required'),
+    lastName : z.string().min(1 , 'Last Name is required'),
+    email : z.string().email().min(1 , 'email is required'),
+    password : z.string().min(8 , 'Password must be atleast 8 characters'),
+    confirmPassword : z.string().min(8)
+}).refine((data) => data.password == data.confirmPassword , {
+    message : "Passwords do not match",
+    path:['confirmPassword']
+})
+
+type FormFields = z.infer<typeof FormSchema>
 
 const LoginInput = () => {
+
+  const {register , 
+    handleSubmit , 
+    setError,
+    formState : {
+        errors , isSubmitting
+    }} = useForm<FormFields>({
+        resolver : zodResolver(FormSchema)
+    })
+
+  const onSubmitHandler = async(data : FormFields) => {
+   try{
+    await new Promise(resolve => setTimeout(resolve , 1000))
+    // throw new Error
+    console.log(data);
+   }catch(err){ 
+    setError('email' , {
+        message : 'The Email is already Taken'
+    })
+}
+  }
+
   return(
     <div className={styles.loginInputCover}>
     <div className={styles.formTitle}>
         <h2>Create an Account</h2>
         <p>Already have and account? <a href="">Log in</a></p>
+  
     </div>    
-    <form > 
+    <form onSubmit={handleSubmit(onSubmitHandler)}> 
      <div className={styles.inputField}>
-     <input  placeholder = 'First Name' type="text" />
-     <input  placeholder ='Last Name'type="text" />
+     <input {...register('firstName')} placeholder = 'First Name' type="text" />
+   
+     <input {...register('lastName')} placeholder ='Last Name'type="text" />
+      
+     </div>
+     <div style={{display : 'flex' , justifyContent: 'space-around' , alignItems:'center'}}>
+     {errors.firstName && (<div className={styles.errorMsg}>{errors.firstName.message}</div>)}
+     {errors.lastName && (<div className={styles.errorMsg}>{errors.lastName.message}</div>)}
+
      </div>
      <div className={styles.inputField}>
-     <input placeholder = 'Email' type="email" />
+     <input {...register('email')}placeholder = 'Email' type="email" />
      </div>
+     {errors.email && (<div className={styles.errorMsg}>{errors.email.message}</div>)}
+
      <div className={styles.inputField}>
-     <input placeholder = 'Password' type="password" />
+     <input {...register('password')} placeholder = 'Password' type="password" />
      </div>
+     {errors.password && (<div className={styles.errorMsg}>{errors.password.message}</div>)}
+
      <div className={styles.inputField}>
-     <input placeholder = 'Confirm Password' type="password" />
+     <input {...register('confirmPassword')} placeholder = 'Confirm Password' type="password" />
      </div>
-      <button type='submit'> Submit</button>
+     {errors.confirmPassword && (<div className={styles.errorMsg}>{errors.confirmPassword.message}</div>)}
+      <button type='submit'> {
+        isSubmitting ? '...Loading' : 'Submit'
+        }</button>
 
     </form>
     <div className={styles.otherSignInCover}>
@@ -29,12 +84,12 @@ const LoginInput = () => {
          <hr style = {{
             flexGrow:1,
             border:'none',
-            // borderTop:'1px solid #ccc',
+            borderTop:'1px solid #ccc',
             margin : '3px'
          }}/> <p>Or register with</p> <hr style = {{
             flexGrow:1,
             border:'none',
-            // borderTop:'1px solid #ccc',
+            borderTop:'1px solid #ccc',
             margin : '3px'
          }}/>
         </div>
--- zod-app/src/types.ts
+++ zod-app/src/types.ts
@@ -2,3 +2,11 @@ export type UserAccType = {
     username : string,
     password : string
 }
+
+export type FormFields = {
+    firstName : string,
+    lastName : string,
+    email : string,
+    password : string,
+    conformPassword : string
+}
